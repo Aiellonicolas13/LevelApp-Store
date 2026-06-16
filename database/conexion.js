77b7e44c879506136064d98484b033283a75c761
rogerllanos <rogerllanos060412@gmail.com>
2026-06-16 18:05:45 -0300

    agrege la conexion a sql

---
+++ database/conexion.js
@@ -0,0 +1,18 @@
+const sql = require('mssql')
+const {configDB} = require('./config')
+const dotenv = require('dotenv')
+dotenv.config();
+
+getSQLConnection = async () => {
+    try {
+        const pool = await sql.connect(configDB)
+
+        const resultadoConexion = await pool.request().query('SELECT * FROM Juegos');
+        console.log(resultadoConexion)
+        return pool
+    } catch (error) {
+        console.log("Error en getSQLConnection:", error)
+    }
+}
+
+getSQLConnection()
