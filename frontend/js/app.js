c2537607f47397ff5b7c063ecf1c80eb4c1d5b2a
Parihuancollo <parihuancollodiego@gmail.com>
2026-06-23 12:09:39 -0300

    Todo funcionando, a mejorar detalles

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,6 @@
 const API = "http://127.0.0.1:3000/juegos";
 
-document.getElementById("btnJuegos").addEventListener("click", obtenerJuegos);
+document.getElementById("btnJuegos").addEventListener("click", mostrarOcultarJuegos);
 
 document.getElementById("btnBuscarJuego").addEventListener("click", buscarJuego);
 
@@ -8,9 +8,93 @@ document.getElementById("btnCrear").addEventListener("click", crearJuego);
 
 document.getElementById("btnActualizar").addEventListener("click", actualizarJuego);
 
-document.getElementById("btnUsuario").addEventListener("click", obtenerJuegosUsuario);
-
 document.getElementById("btnEliminar").addEventListener("click", eliminarJuego);
+
+document.getElementById("btnCompras").addEventListener("click", mostrarOcultarCompras);
+
+document.getElementById("btnBuscarCompra").addEventListener("click", buscarCompra);
+
+
+let juegosMostrados = false;
+let comprasVisibles = false;
+
+async function mostrarOcultarJuegos() {
+
+    const contenedor =
+        document.getElementById("listaJuegos");
+
+    const boton =
+        document.getElementById("btnJuegos");
+
+
+    if (!juegosMostrados) {
+
+
+        await obtenerJuegos();
+
+
+        contenedor.style.display = "grid";
+
+
+        boton.textContent = "Ocultar Juegos";
+
+
+        juegosMostrados = true;
+
+
+    } else {
+
+
+        contenedor.style.display = "none";
+
+
+        boton.textContent = "Cargar Juegos";
+
+
+        juegosMostrados = false;
+
+    }
+
+}
+
+async function mostrarOcultarCompras() {
+
+    const contenedor =
+        document.getElementById("listaCompras");
+
+    const boton =
+        document.getElementById("btnCompras");
+
+
+    if (!comprasVisibles) {
+
+
+        await obtenerCompras();
+
+
+        contenedor.style.display = "grid";
+
+
+        boton.textContent = "Ocultar Compras";
+
+
+        comprasVisibles = true;
+
+
+    } else {
+
+
+        contenedor.style.display = "none";
+
+
+        boton.textContent = "Cargar Compras";
+
+
+        comprasVisibles = false;
+
+    }
+
+}
 
 function mostrarError() {
     const mensaje = document.getElementById("mensajeError");
@@ -20,6 +104,21 @@ function mostrarError() {
     setTimeout(() => {
         mensaje.style.display = "none";
     }, 5000); 
+}
+function mostrarErrorCompra() {
+
+    const mensaje = document.getElementById("mensajeErrorCompra");
+
+
+    mensaje.style.display = "block";
+
+    setTimeout(() => {
+
+        mensaje.style.display = "none";
+
+    }, 3000);
+
+
 }
 
 async function obtenerJuegos() {
@@ -32,19 +131,11 @@ async function obtenerJuegos() {
             contenedor.innerHTML += `
                 <div class="game-card">
                     <div class="game-info">
-                        <p class="idJuego">
-                            ID: ${juego.IdJuego}
-                        </p>
+                        <p class="idJuego">ID: ${juego.IdJuego}</p>
                         <h3>${juego.Nombre}</h3>
-                        <p class="price">
-                            $${juego.Precio}
-                        </p>
-                        <p class="stock">
-                            Stock: ${juego.Stock}
-                        </p>
-                        <p>
-                            Categoría: ${juego.IdCategoria}
-                        </p>
+                        <p class="price">$${juego.Precio}</p>
+                        <p class="stock">Stock: ${juego.Stock}</p>
+                        <p>Categoría: ${juego.IdCategoria}</p>
                     </div>
                 </div>
             `;
@@ -71,7 +162,7 @@ async function buscarJuego() {
                 <h3>${juego.Nombre}</h3>
                 <p>Precio: $${juego.Precio}</p>
                 <p>Stock: ${juego.Stock}</p>
-                <p>Categoria: ${juego.IdCategoria}</p>
+                <p>Categoria: ${juego.Categoria}</p>
             </div>
         `;
     } catch (error) {
@@ -226,5 +317,76 @@ async function obtenerJuegosUsuario() {
     } catch (error) {
         console.log(error);
         alert("Error al obtener juegos del usuario");
+    }
+}
+
+async function obtenerCompras() {
+    try {
+        const response = await fetch(
+            "http://127.0.0.1:3000/compras"
+        );
+        if (!response.ok) {
+            throw new Error();
+        }
+        const compras = await response.json();
+        const contenedor = document.getElementById("listaCompras");
+        contenedor.innerHTML = "";
+        compras.forEach(compra => {
+            contenedor.innerHTML += `
+            <div class="game-card">
+                <div class="game-info">
+                    <p class="idJuego">
+                        Compra ID: ${compra.IdCompra}
+                    </p>
+                    <p>
+                        Usuario: ${compra.Usuario}
+                    </p>
+                    <p class="price">
+                        Total: ${compra.Total} $
+                    </p>
+                    <p>
+                        Fecha: ${compra.FechaCompra}
+                    </p>
+                </div>
+            </div>`;
+        });
+    } catch(error) {
+        console.log(error);
+        alert(
+            "Error al cargar compras"
+        );
+    }
+}
+
+async function buscarCompra() {
+
+    try {
+        const id = document.getElementById("idCompraJuego").value;
+        if (!id) {
+            alert("Ingrese un ID de juego");
+            return;
+        }
+        const response = await fetch(`http://127.0.0.1:3000/compras/${id}`);
+        if (!response.ok) {
+            throw new Error("Compra no encontrada");
+        }
+        const datos = await response.json();
+        const contenedor = document.getElementById("compraEncontrada");
+        contenedor.innerHTML = "";
+        const compras = Array.isArray(datos) ? datos: [datos];
+        compras.forEach(compra => { contenedor.innerHTML += `
+            <div class="user-card">
+                <h3>Compra ID: ${compra.IdJuego}</h3>
+                <p>Juego: ${compra.Nombre}</p>
+                <p>Cantidad: ${compra.TotalUnidadesVendidas}</p>
+                <p>Total ingresos: ${compra.TotalIngresos}</p>
+            </div>`;
+        });
+    } catch(error) {
+        console.log(error);        
+        mostrarErrorCompra();
+        const contenedor = document.getElementById("compraEncontrada");
+        contenedor.innerHTML = "";
+        // alert("No se encontraron compras");
     }
 }
