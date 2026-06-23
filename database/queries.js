9856a66e4f29b2285ecb47fa998b414d7b52789f
Gi na <ginacascio123@gmail.com>
2026-06-23 20:58:52 -0300

    Agrego query del put de juegos

--- database/queries.js
+++ database/queries.js
@@ -55,4 +55,13 @@ module.exports = {
         INNER JOIN DetalleComprasJuegos dc
         ON c.IdCompra = dc.IdCompra
         WHERE MONTH(c.FechaCompra) = @mes;`,
+
+        updateJuegoCompleto: `
+UPDATE Juegos
+SET Nombre = @Nombre,
+    Precio = @Precio,
+    Stock = @Stock,
+    IdCategoria = @IdCategoria
+WHERE IdJuego = @IdJuego
+`,
 }
